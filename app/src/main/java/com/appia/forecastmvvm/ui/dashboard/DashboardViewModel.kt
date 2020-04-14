da06153f09f9b5b90319de79d7178920e8efab4a
Evert Moreno <evernext10@gmail.com>
2020-04-14 11:10:55 -0500

    First commit, to begin this project

---
+++ app/src/main/java/com/appia/forecastmvvm/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.appia.forecastmvvm.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
